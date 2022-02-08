697c4596987baaccc7bfa9e75766b0e307d46f0d
akib22 <adnanakib75@gmail.com>
2022-02-08 21:11:53 +0600

    Add user model

---
+++ models/User.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+
+const { Schema } = mongoose;
+
+const userSchema = new Schema(
+  {
+    name: { type: String, required: true },
+    password: { type: String, required: true },
+    phoneNumber: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    wishlists: [{ type: Schema.Types.ObjectId, ref: 'Product' }],
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model('User', userSchema);
